15baf3df298d5c89cad85f0ea989409ad37ecfc0
Ivan Kh. <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-30 14:33:13 +0300

    Forgot the import

--- src/main/java/net/mchorizons/semblance/mixin/MinecraftServerMixin.java
+++ src/main/java/net/mchorizons/semblance/mixin/MinecraftServerMixin.java
@@ -1,7 +1,7 @@
 package net.mchorizons.semblance.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.mchorizons.semblance.Config;
-import net.mchorizons.semblance.MinecraftServerInterface;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
