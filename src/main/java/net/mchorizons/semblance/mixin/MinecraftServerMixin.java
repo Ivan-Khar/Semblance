096a06e81726ebd2211a563df4e0d72c270f6b43
Ivan Kh. <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-30 14:09:18 +0300

    Use proper mixin method to replace the return value. Also ditch vanilla and forge checks

--- src/main/java/net/mchorizons/semblance/mixin/MinecraftServerMixin.java
+++ src/main/java/net/mchorizons/semblance/mixin/MinecraftServerMixin.java
@@ -4,11 +4,12 @@ import net.mchorizons.semblance.Config;
 import net.mchorizons.semblance.MinecraftServerInterface;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin implements MinecraftServerInterface {
-	@Override
-	public String getServerModName() {
+	@ModifyReturnValue(method = "getServerModName", at = @At("RETURN"))
+	public String replaceServerModName(String original) {
 		return Config.getServerBrand();
 	}
 }
